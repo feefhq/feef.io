d8330f792118ab7986a78eb7577871e1771c94c4
philp <phil@feef.io>
2022-08-25 22:14:25 +0100

    Add doc title

--- components/Layout.js
+++ components/Layout.js
@@ -1,8 +1,12 @@
+import Head from 'next/head'
 import Header from './Header'
 
 const Layout = ({ children }) => {
   return (
     <>
+      <Head>
+        <title>Feef.io</title>
+      </Head>
       <Header />
       <main>{children}</main>
     </>
